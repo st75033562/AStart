c7ddf82a744bdedb90297c628dd4a2bae19c49fd
xiaxiaohu <xxh@g3games.cn>
2019-11-28 18:26:11 +0800

    init

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour
+{
+    public delegate void VoidDelegate(int x, int y);
+    public VoidDelegate FindPath;
+
+    private void OnMouseDown()
+    {
+        if (FindPath != null)
+        {
+            FindPath((int)this.transform.position.x, (int)this.transform.position.y);
+        }
+    }
+}
